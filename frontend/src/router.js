c1651c3a9f7e7c468689e0d37b997396950e0a26
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-20 23:21:18 +0100

    create front and backend, start with creation of routes for the frontend components

---
+++ frontend/src/router.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from "vue-router";
+import DefaultLayout from "./components/DefaultLayout.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: DefaultLayout,
+    children: [
+      {
+        path: "/",
+        name: "Home",
+        component: Home,
+      },
+      {
+        path: "/images",
+        name: "MyImages",
+        component: MyImages,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
